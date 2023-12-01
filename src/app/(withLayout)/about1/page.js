670c77a140796f2074c9d8905b986379d12bd8de
raselsharif <raselsharif9@gmail.com>
2023-12-01 22:37:51 +0600

    added link/next and meta data

--- src/app/(withLayout)/about1/page.js
+++ src/app/(withLayout)/about1/page.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+export const metadata = {
+  title: "About one",
+  description: "This is about page",
+};
 const AboutPage = () => {
   return (
     <div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h2>Home Page</h2>
+      <Link href={"/about1"}>
+        <button className="border px-3 py-2 rounded-md">about one</button>
+      </Link>
     </div>
   );
 }
